341edc55f42929580d6749802cb455a5498fea0a
Brijesh S <satasiyabrijesh0412@gmail.com>
2025-02-27 18:29:10 +0530

    Thu Feb 27 06:29:10 PM IST 2025

--- 05_Recursion/reverseArray.js
+++ 05_Recursion/reverseArray.js
@@ -26,23 +26,23 @@ function swapElement(arr,e1,e2){
     arr[e2]=temp;
 }
 
-function reverseTwoArray(arr,left,right) {     // reverse using two pointer and recursion
+function reverseArrayUsingTwoPointer(arr,left,right) {     // reverse using two pointer and recursion
     if(left >= right){
         console.log(arr);
         return;
     }
     swapElement(arr,left,right);
-    reverseTwoArray(arr,left+1,right-1);
+    reverseArrayUsingTwoPointer(arr,left+1,right-1);
 }
 
-// reverseTwoArray([1,2,1,4,3],0,4);
+// reverseArrayUsingTwoPointer([1,2,1,4,3],0,4);
 
-function reverseArrayOnePointer(arr,left) {     // reverse using one pointer and recursion
+function reverseArrayUsingOnePointer(arr,left) {     // reverse using one pointer and recursion
     if(left >= arr.length-left-1) {
         return arr
     }
     swapElement(arr,left,arr.length-left-1);
-    return reverseArrayOnePointer(arr,left+1);
+    return reverseArrayUsingOnePointer(arr,left+1);
 }
 
-console.log(reverseArrayOnePointer([1,2,1,4,3],0));
+console.log(reverseArrayUsingOnePointer([1,2,1,4,3],0));
